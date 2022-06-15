4dfe63bf12822f063e2d89d8f9bddd6bc806d10d
EgorSokolov <esokolov@prooftechit.com>
2022-06-15 22:58:26 +0200

    array_of_tuples_hw

--- lesson4.playground/Contents.swift
+++ lesson4.playground/Contents.swift
@@ -1,4 +1,18 @@
 import UIKit
 
-var greeting = "Hello, playground"
-let gitHub = "gitHub"
+//1. Создать аккаунт на гитхабе и скинуть туда д/з
+//2. Дан массив, элементы которого это картежи с типами данных Int и String. Необходимо:
+//- Возвести Int в квадрат
+//- Отфильтровать только четные элементы
+//- Упорядочить строки по алфавитному возрастанию, можно обозначить букву на любом языке.
+//
+//p.s алфовитное возрастание - это когда начинается с a и идет потом б, в, г, д и т.д
+//
+//Соответсвенно на англисйком начинается с a, b, c, d и заканчивается x,y,z.
+
+let arrayOfTuples: [(Int, String)] = [(11, "NewYork"), (12, "Lissabon"), (22, "Moscow"), (42, "Saint-P"), (36, "Paris"), (54, "Berlin"), (166,"Astana")]
+
+let formatedArray = arrayOfTuples.map{(Int(pow(Double($0.0), 2)), $0.1)}.filter {$0.0.isMultiple(of: 2)}.sorted { $0.1 < $1.1}
+
+print(formatedArray)
+
